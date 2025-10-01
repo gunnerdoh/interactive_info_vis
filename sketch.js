b3ee9b0f64688e7150b671f7a1651ff0f9c7aae1
Gunner Dohrenwend <139017216+gunnerdoh@users.noreply.github.com>
2025-10-01 15:40:01 -0700

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,49 +1,49 @@
 //custom variables for y-coordinate of sun & horizon
 let shapeHeight;
 
-let designWidth = 400;
-let designHeight= 400;
+let designWidth = 1500;
+let designHeight= 1200;
 let horizon ;
 function setup() {
-  createCanvas(designWidth,designHeight);
-  horizon =height/2;
+   createCanvas(designWidth,designHeight);
+  horizon =height/2;
 }
 
 function draw() {
 
-  //shape follows y-coordinate of mouse
-  shapeHeight = mouseY;
+  //shape follows y-coordinate of mouse
+ shapeHeight = mouseY;
   currentWidth = mouseX;
 
-  //light blue background if the shape is above horizon
+  //light blue background if the shape is above horizon
 
   //with if-else statement
   if (shapeHeight < horizon) {
-    background("lightblue"); // blue if above horizon
+   background("lightblue"); // blue if above horizon
     
   } else {
-    background("grey"); // grey if below horizon
+    background("darkblue"); // grey if below horizon
   }
 
-  //sun
-  fill("white");
+  //sun
+  fill("white");
   
-  rect(width/4, shapeHeight, width/2);
+  rect(width/4, shapeHeight, width/2);
   textSize(20);
   fill("black");
-  text('Hi! My name is...', currentWidth/2, shapeHeight/2);
+  text('Hi! My name is Gunner Dohrenwend', currentWidth/2, shapeHeight/2);
   
 
 
-  // draw line for horizon
-  stroke('lavender');
-  line(0,horizon,width,horizon);
+  // draw line for horizon
+  stroke('black');
+  line(0,horizon,width,horizon);
 
-  //grass
+  //grass
+ 
+  fill("green");
 
-  fill("lavender");
-
-  rect(0, horizon, width, height);
+  rect(0, horizon, width, height);
 
 }
 
